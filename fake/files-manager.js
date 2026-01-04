7db3b6b55310e80e25c6996d37822855eb43fdff
gebeto <slavik.nychkalo@gmail.com>
2026-01-04 19:19:46 +0200

    add files endpoint

---
+++ fake/files-manager.js
@@ -0,0 +1,115 @@
+import { defineFakeRoute } from "vite-plugin-fake-server/client";
+
+const dummyData = {
+  "/": [
+    {
+      name: "book.epub",
+      size: 2953653,
+      isDirectory: false,
+      isEpub: true,
+    },
+    {
+      name: "Wallpapers",
+      size: 0,
+      isDirectory: true,
+      isEpub: false,
+    },
+    {
+      name: "sleep",
+      size: 0,
+      isDirectory: true,
+      isEpub: false,
+    },
+    {
+      name: "nested-1",
+      size: 0,
+      isDirectory: true,
+      isEpub: false,
+    },
+    {
+      name: "empty-folder",
+      size: 0,
+      isDirectory: true,
+      isEpub: false,
+    },
+    {
+      name: "some_test_ebook.epub",
+      size: 1658321,
+      isDirectory: false,
+      isEpub: true,
+    },
+    {
+      name: "some_another ebook for test.epub",
+      size: 3050316,
+      isDirectory: false,
+      isEpub: true,
+    },
+    {
+      name: "test.bmp",
+      size: 3050316,
+      isDirectory: false,
+      isEpub: false,
+    },
+  ],
+  "/nested-1": [
+    {
+      name: "nested-2",
+      size: 0,
+      isDirectory: true,
+      isEpub: false,
+    },
+  ],
+  "/nested-1/nested-2": [
+    {
+      name: "nested-3",
+      size: 0,
+      isDirectory: true,
+      isEpub: false,
+    },
+  ],
+  "/empty-folder": [],
+};
+
+export const routes = [
+  {
+    url: "/api/status",
+    response: () => {
+      return [];
+    },
+  },
+  {
+    url: "/api/files",
+    response: (data) => {
+      return dummyData[data.path];
+    },
+  },
+  {
+    url: "/api/upload",
+    response: () => {
+      return "Ok";
+    },
+  },
+  {
+    url: "/api/mkdir",
+    response: () => {
+      return "Ok";
+    },
+  },
+  {
+    url: "/api/move",
+    response: () => {
+      return "Ok";
+    },
+  },
+  {
+    url: "/api/delete",
+    response: () => {
+      return "Ok";
+    },
+  },
+].reduce((acc, route) => {
+  return {
+    ...acc,
+    [route.url]: { response: route.response },
+  };
+}, {});
--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 #!/usr/bin/env node
 
 import { createServer } from "vite";
-import { vitePluginFakeServer } from "vite-plugin-fake-server";
+import express from "express";
+import { routes as filesRoutes } from "./fake/files-manager.js";
+
+const rootFile = "HomePage.html";
 
 async function startDevServer() {
+  const app = express();
+
   const port = 3000;
   const server = await createServer({
     root: process.cwd(),
+    appType: "spa",
     server: {
       port: port,
-      open: "HomePage.html",
+      open: rootFile,
+      middlewareMode: true,
     },
-    plugins: [vitePluginFakeServer()],
   });
 
-  await server.listen();
+  // await server.listen();
 
-  console.log(`Crosspoint dev server running at http://localhost:${port}`);
+  // filesRoutes.forEach(({ url, response }) => {
+  //   app.use(url, async (_req, res) => {
+  //     const resp = response(_req.params);
+  //     res.json(resp);
+  //   });
+  // });
+
+  app.get("/", async (_req, res) => {
+    return res.redirect("/HomePage.html");
+  });
+
+  app.get("/api/files", async (req, res) => {
+    return res.json(filesRoutes["/api/files"].response(req.query));
+  });
+
+  app.get("/api/status", async (req, res) => {
+    return res.json(filesRoutes["/api/status"].response(req.query));
+  });
+
+  app.use(server.middlewares);
+
+  app.listen(port, () => {
+    console.log(`Crosspoint dev server running at http://localhost:${port}`);
+  });
 }
 
 startDevServer();
