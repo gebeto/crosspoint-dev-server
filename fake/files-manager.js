22573e20655718986f8dd429f49ceb6439de65f3
gebeto <slavik.nychkalo@gmail.com>
2026-01-04 22:46:53 +0200

    add form data

--- fake/files-manager.js
+++ fake/files-manager.js
@@ -1,4 +1,6 @@
-const dummyData = {
+import path from "path";
+
+export const allFiles = {
   "/": [
     {
       name: "book.epub",
@@ -65,6 +67,7 @@ const dummyData = {
       isEpub: false,
     },
   ],
+  "/nested-1/nested-2/nested-3": [],
   "/empty-folder": [],
 };
 
@@ -72,13 +75,20 @@ export const routes = [
   {
     url: "/api/status",
     response: () => {
-      return [];
+      return {
+        version: "0.0.0-dev",
+        ip: "0.0.0.0",
+        mode: "STA",
+        rssi: 99,
+        freeHeap: 123456,
+        uptime: 10,
+      };
     },
   },
   {
     url: "/api/files",
     response: (data) => {
-      return dummyData[data.path];
+      return allFiles[data.path] ?? [];
     },
   },
   {
@@ -89,20 +99,49 @@ export const routes = [
   },
   {
     url: "/api/mkdir",
-    response: () => {
-      return "Ok";
+    response: (data) => {
+      const dirPath = path.join(data.path, data.name);
+      allFiles[dirPath] = [];
+      allFiles[data.path].push({
+        name: data.name,
+        size: 0,
+        isDirectory: true,
+        isEpub: false,
+      });
+
+      return dirPath;
     },
   },
   {
     url: "/api/move",
-    response: () => {
-      return "Ok";
+    response: (data) => {
+      const oldPath = data.path;
+      const oldDir = path.dirname(oldPath);
+      const fileName = path.basename(oldPath);
+
+      const newPath = data.new_path;
+      const newDir = path.dirname(newPath);
+      const newFileName = path.basename(newPath);
+
+      const file = allFiles[oldDir].find((f) => f.name === fileName);
+      file.name = newFileName;
+
+      if (!allFiles[newDir]) {
+        throw new Error("Destination directory does not exist");
+      }
+
+      allFiles[oldDir] = allFiles[oldDir].filter((f) => f !== file);
+      allFiles[newDir] = [...allFiles[newDir], file];
     },
   },
   {
     url: "/api/delete",
-    response: () => {
-      return "Ok";
+    response: (data) => {
+      const deletePath = data.path;
+      const dir = path.dirname(deletePath);
+      const base = path.basename(deletePath);
+
+      allFiles[dir] = allFiles[dir].filter((f) => f.name !== base);
     },
   },
 ].reduce((acc, route) => {
--- index.js
+++ index.js
@@ -3,16 +3,22 @@
 import { createServer } from "vite";
 import express from "express";
 import { routes as filesRoutes } from "./fake/files-manager.js";
+import formData from "express-form-data";
+import path from "path";
+import os from "os";
 
 const rootFile = "HomePage.html";
 
 async function startDevServer() {
   const app = express();
+  app.use(formData.parse());
 
   const port = 3000;
+  const cacheDir = path.join(os.tmpdir(), "crosspoint-dev-server-cache");
   const server = await createServer({
     root: process.cwd(),
     appType: "spa",
+    cacheDir: cacheDir,
     server: {
       port: port,
       open: rootFile,
@@ -20,17 +26,18 @@ async function startDevServer() {
     },
   });
 
-  app.use(express.json()); // for parsing application/json
-  app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
   app.get("/", async (_req, res) => {
     return res.redirect("/HomePage.html");
   });
+
   app.get("/files", async (_req, res) => {
     return res.redirect("/FilesPage.html");
   });
 
   app.get("/api/files", async (req, res) => {
+    res.removeHeader("ETag");
+    res.removeHeader("Last-Modified");
+
     return res.json(filesRoutes["/api/files"].response(req.query));
   });
 
@@ -39,11 +46,26 @@ async function startDevServer() {
   });
 
   app.post("/api/delete", async (req, res) => {
-    console.log(req);
-    return res.json({});
+    res.json(filesRoutes["/api/delete"].response(req.body));
+
+    return res.sendStatus(200).end();
+  });
+
+  app.post("/api/move", async (req, res) => {
+    res.json(filesRoutes["/api/move"].response(req.body));
+
+    return res.sendStatus(200).end();
+  });
+
+  app.post("/api/mkdir", async (req, res) => {
+    res.json(filesRoutes["/api/mkdir"].response(req.body));
+
+    return res.sendStatus(200).end();
   });
 
   app.use(server.middlewares);
+
+  app.disable("etag");
 
   app.listen(port, () => {
     console.log(`Crosspoint dev server running at http://localhost:${port}`);
